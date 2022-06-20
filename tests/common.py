1d1146f4282b2ffc7a2a647d2771537d9300ec9e
SergeyNikAl <cergei_nikulin@mail.ru>
2022-06-20 14:25:26 +0300

    defined skeleton for models and README for models

--- tests/common.py
+++ tests/common.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from users.models import User
+
 
 def create_users_api(admin_client):
     data = {

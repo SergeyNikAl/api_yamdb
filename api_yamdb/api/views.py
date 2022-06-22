5dc7fa7e443b31e3f988df315c1166c55a6206b2
Татьяна Малявко <t.d.malyavko@yandex.ru>
2022-06-22 16:02:26 +0500

    1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from requests import Response
+from rest_framework import permissions, viewsets, status
 
-# Create your views here.
+from  reviews.models import User
+from .permissions  import IsAdmin
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    search_fields = ('=username',)
+
+    @action(
+        detail=False,
+        methods=['GET', 'PATCH'],
+        permission_classes=(IsAdmin,)
+    )
+    def me(self, request):
+        if request.method == 'PATCH':
+            serializer = UserSerializer(
+                request.user, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save(role=request.user.role)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)

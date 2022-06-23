143cddfd41a3879c9582d33775e1a603a2af467a
Татьяна Малявко <t.d.malyavko@yandex.ru>
2022-06-23 12:25:17 +0500

    3

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,12 +5,17 @@ from rest_framework.decorators import action
 from reviews.models import User
 
 from .permissions import IsAdmin
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TokenSerializer, SignUpSerializer
+from api_yamdb.api import serializers
+
 
 USER = 'user'
 MODERATOR = 'moderator'
 ADMIN = 'admin'
-
+USERNAME_ALREADY_EXISTS = 'Такое имя уже занято.'
+EMAIL_ALREADY_EXISTS = 'Такая почта уже зарегестрирована'
+CORRECT_CODE_EMAIL_MESSAGE = 'Код подтверждения: {code}'
+INVALID_CODE = 'Неизвестный запрос'
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -42,3 +47,56 @@ class UserViewSet(viewsets.ModelViewSet):
         else:
             serializer.save(role=user.role)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SingUpViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request):
+        serializers = SignUpSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        username = serializers.validated_data.get('username')
+        email = serializers.validated_data.get('email')
+        try:
+            user = User.objects.get(
+                username=username,
+                email=email
+            )
+        except User.DoesNotExist:
+            if User.objects.filter(username=username).exists():
+                return Response(USERNAME_ALREADY_EXISTS, status=status.HTTP_400_BAD_REQUEST)
+            if User.objects.filter(email=email).exists():
+                return Response(EMAIL_ALREADY_EXISTS, status=status.HTTP_400_BAD_REQUEST)
+            user = User.objects.create_user(username=username, email=email)
+        user.save()
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            subject='YaMDb registration code',
+            message=CORRECT_CODE_EMAIL_MESSAGE.format(code=confirmation_code),
+            from_email=None,
+            recipient_list=[user.email],
+        )
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+        
+class TokenView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request):
+        serializers = TokenSerializer
+        serializers.is_valid(raise_exception=True)
+        user = get_object_or_404(User, username=serializers.validated_data.get('username'))
+        confirmation_code = serializers.validated_data.get('confirmation_code')
+        if not default_token_generator.check_token(user=user, token=confirmation_code):
+            return Response(
+                INVALID_CODE,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        user.save()
+        jwt_payload_hendler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODEHANDLER
+        payload = jwt_payload_hendler(request.user)
+        token = jwt_encode_handler(payload)
+        return Response({'token': token}, status=status.HTTP_200_OK)
